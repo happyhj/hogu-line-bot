7c8f5ab25bc60f84144c6e999349a53817c8f0d3
unknown <>
2017-01-04 23:11:16 +0900

    error fix

--- app.py
+++ app.py
@@ -69,6 +69,12 @@ parser = WebhookParser(channel_secret)
 
 firebase = firebase.FirebaseApplication('https://hogu-line-bot.firebaseio.com', None)
 
+result = firebase.get('/customSticker', '인사')
+
+print result
+
+result = json.loads(result)
+
 def props(x):
     return dict((key, getattr(x, key)) for key in dir(x) if key not in dir(x.__class__))
 
@@ -180,12 +186,8 @@ def callback():
             query = tokens[1]
             result = firebase.get('/customSticker', query)
 
-            print result
+            print result[0]
 
-            result = json.loads(result)
-
-            packageId = result[packageId]
-            stickerId = result[stickerId]
 
             line_bot_api.reply_message(
                 event.reply_token,
