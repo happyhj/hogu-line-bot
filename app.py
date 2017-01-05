6973342721fe7c30f0ee0c98afb041e2eacbc687
Heejae Kim <heejae.kim@navercorp.com>
2017-01-05 13:06:23 +0900

    리스트 삭제 기능 추가

--- app.py
+++ app.py
@@ -206,10 +206,10 @@ def callback():
                 for idx, stickerInfo in enumerate(stickerList):
                     packageId = stickerInfo.get('packageId')
                     stickerId = stickerInfo.get('stickerId')
-                    print 'https://sdl-stickershop.line.naver.jp/products/0/0/1/'+packageId+'/android/stickers/'+stickerId+'.png'
-                    print alias
-                    print str(idx)
-                    print '@stk.remove '+alias+ ' '+ packageId + ' ' + stickerId
+                    # print 'https://sdl-stickershop.line.naver.jp/products/0/0/1/'+packageId+'/android/stickers/'+stickerId+'.png'
+                    # print alias
+                    # print str(idx)
+                    # print '@stk.remove '+alias+ ' '+ packageId + ' ' + stickerId
                     carouselColumnArray.append(
                         CarouselColumn(
                             thumbnail_image_url='https://sdl-stickershop.line.naver.jp/products/0/0/1/'+packageId+'/android/stickers/'+stickerId+'.png',
